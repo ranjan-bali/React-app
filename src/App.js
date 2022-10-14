8eaacd526a61ff7d996000ac770052ba10e73261
ranjan-bali <ranjan1681.be202chitkara.edu.in>
2022-10-14 10:07:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./form.js";
+
+function App() {
+  return (
+    <>
+      <Form></Form>
+    </>
+  );
+}
+
+export default App;
